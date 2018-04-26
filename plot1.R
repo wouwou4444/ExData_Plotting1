af4164f1f7d97546125113f40e4362668d581df8
wouwou4444 <valery.buz@outlook.com>
2018-04-26 18:42:57 +0200

    ex1

---
+++ plot1.R
@@ -0,0 +1,37 @@
+#### Assignment JH Course 4 - week 1
+
+setwd(dir = "d:/machine-learning/Johns_Hopkins/course4/week1/")
+
+if (! file.exists("./data")) {
+  dir.create("./data")
+}
+
+file.exists("./data")
+setwd(dir = "./data/")
+
+urlFile <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destFile <- "./household_power_consumption.zip"
+
+download.file(urlFile, destfile = destFile)
+unzip(destFile)
+list.files()
+
+hpc_data <- read.table(file = "./household_power_consumption.txt", 
+                       header = TRUE,
+                       na.strings = "?",
+                       sep = ";")
+object.size(hpc_data)
+
+# strptime(head(hpc_data$Time),format = "%H:%M:%S")
+# strptime(head(hpc_data$Date),format = "%d/%m/%Y")
+hpc_data$date_time <- (paste(sep = " ", hpc_data$Date, hpc_data$Time))
+
+hpc_data$date_time <- strptime(hpc_data$date_time, format = "%d/%m/%Y %H:%M:%S")
+hpc <- hpc_data[hpc_data$Date == "1/2/2007" | hpc_data$Date == "2/2/2007",]
+nrow(hpc)
+
+### plot 1
+dev.copy(png, file = "plot1.png")
+par(mfrow = c(1, 1))
+hist(as.numeric(hpc$Global_active_power),breaks = 12, col = "red",xlab = "Global Active Power(kilowatts)")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,47 @@
+#### Assignment JH Course 4 - week 1
+
+setwd(dir = "d:/machine-learning/Johns_Hopkins/course4/week1/")
+
+if (! file.exists("./data")) {
+  dir.create("./data")
+}
+
+file.exists("./data")
+setwd(dir = "./data/")
+
+urlFile <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destFile <- "./household_power_consumption.zip"
+
+download.file(urlFile, destfile = destFile)
+unzip(destFile)
+list.files()
+
+hpc_data <- read.table(file = "./household_power_consumption.txt", 
+                       header = TRUE,
+                       na.strings = "?",
+                       sep = ";")
+object.size(hpc_data)
+
+# strptime(head(hpc_data$Time),format = "%H:%M:%S")
+# strptime(head(hpc_data$Date),format = "%d/%m/%Y")
+hpc_data$date_time <- (paste(sep = " ", hpc_data$Date, hpc_data$Time))
+
+hpc_data$date_time <- strptime(hpc_data$date_time, format = "%d/%m/%Y %H:%M:%S")
+hpc <- hpc_data[hpc_data$Date == "1/2/2007" | hpc_data$Date == "2/2/2007",]
+nrow(hpc)
+
+
+### plot 4
+dev.copy(png, file = "plot4.png")
+par(mfrow = c(2, 2))
+with(hpc, {
+  plot(date_time, Global_active_power, type= "l",xlab = "", ylab = "Global Active Power")
+  plot(date_time, Voltage, type= "l", xlab = "datetime")
+  plot(date_time, Sub_metering_1, type = "l", xlab = "", ylab = "Energy submetering")
+  lines(date_time, Sub_metering_2, col = "red")
+  lines(date_time, Sub_metering_3, col = "blue")
+  legend("topright", lty = 1, seg.len = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(date_time, Global_reactive_power, type= "l",xlab = "datetime")
+}
+)
+dev.off()
